d48802f8d1f5796c902cec7ba49c66ef0c8d2b30
vagrant <vagrant@ironhack>
2014-10-25 09:59:46 +0000

    final arranges

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,23 @@
-i = 0
-num <= 100
+i = 1
+num = 100
 
 while i < num do 
-	if 
+	if i % 3 == 0 && i % 5 == 0
+		puts "Fizzbuzz"
+
+	elsif i % 5 == 0
+		puts "Buzz"
+
+	elsif i % 3 == 0
+		puts "Fizz"
+
+	else 
+		puts i
+	end
+
+	i +=1
+
 end 
+
+
 
